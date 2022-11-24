9e433dbf4a0a8b12d8ace58a155bd5b25e440e75
Alessandro Coppola <102584241+alecoppola1272@users.noreply.github.com>
2022-11-24 17:37:45 +0100

    histograms filled and writed on a file

--- simulation.cpp
+++ simulation.cpp
@@ -13,7 +13,7 @@ R__LOAD_LIBRARY(ParticleType_cpp.so);
 R__LOAD_LIBRARY(ResonanceType_cpp.so);
 R__LOAD_LIBRARY(Particle_cpp.so);
 
-void macro2()
+void macro()
 {
 
     Particle::AddParticleType("pi+", 0.13957, 1, 0);    // index 0 , positive pion
@@ -27,38 +27,25 @@ void macro2()
     gRandom->SetSeed();
     std::vector<Particle> EventParticles;
     std::vector<Particle> ResonanceEvents;
-    TH1F *invmass5 =
-        new TH1F("inv mass5", "Invariant Mass Distribution (decay)", 300, 0, 2);
-    TH1F *types =
-        new TH1F("types", "Abundancies of generated Particles", 7, 0, 7);
-    TH2F *angles =
-        new TH2F("Angles", "Distribution of azimutal and polar angles", 100, 0, 2 * M_PI, 100, 0, M_PI);
-    TH1F *pav =
-        new TH1F("pav", "Average impulse Distribution", 1000, 0, 7);
-    TH1F *Impulse =
-        new TH1F("Impulse", "Distribution of trasverse impulse", 1000, 0, 5);
-    TH1F *energy = new TH1F("energy", "Distribution of energy", 1000, 0, 7);
-    TH1F *invmass0 =
-        new TH1F("inv mass0", "Invariant Mass Distribution", 1000, 0, 7);
-    TH1F *invmass1 =
-        new TH1F("inv mass1", "Invariant Mass Distribution (opposite charges)",
-                 100, 0.75, 1.05);
-    TH1F *invmass2 =
-        new TH1F("inv mass2", "Invariant Mass Distribution (same charges)", 100,
-                 0.75, 1.05);
-    TH1F *invmass3 = new TH1F(
-        "inv mass3", "Invariant Mass Distribution (p+/k- or p-/k+)", 1000, 0, 7.);
-    TH1F *invmass4 = new TH1F(
-        "inv mass4", "Invariant Mass Distribution (p+/k+ or p-/k-)", 1000, 0, 7.);
+    TH1F *htypes = new TH1F("types", "Abundancies of generated Particles", 7, 0, 7);
+    TH2F *hangles = new TH2F("Angles", "Distribution of azimutal and polar angles", 100, 0, 2 * M_PI, 100, 0, M_PI);
+    TH1F *hpav = new TH1F("pav", "Average impulse Distribution", 1000, 0, 7);
+    TH1F *hImpulse = new TH1F("Impulse", "Trasverse impulse Distribution", 1000, 0, 5);
+    TH1F *henergy = new TH1F("energy", "Energy Distribution", 1000, 0, 7);
+    TH1F *hinvmass0 = new TH1F("inv mass0", "Invariant Mass Distribution", 1000, 0, 7);
+    TH1F *hinvmass1 = new TH1F("inv mass1", "Invariant Mass Distribution (opposite charges)", 100, 0.75, 1.05);
+    TH1F *hinvmass2 = new TH1F("inv mass2", "Invariant Mass Distribution (same charges)", 100, 0.75, 1.05);
+    TH1F *hinvmass3 = new TH1F("inv mass3", "Invariant Mass Distribution (p+/k- or p-/k+)", 1000, 0, 7.);
+    TH1F *hinvmass4 = new TH1F("inv mass4", "Invariant Mass Distribution (p+/k+ or p-/k-)", 1000, 0, 7.);
+    TH1F *hinvmass_decay = new TH1F("inv mass decay", "Invariant Mass Distribution (decay)", 300, 0, 2);
 
-    double average_p = gRandom->Exp(1.);
-
-    for (int i = 0; i < 1E5; i++) // 10E5
+    for (int i = 0; i < 10; i++)
     {
-        for (int j = 1; j <= 100; ++j)
+        for (int j = 1; j <= 1000; ++j)
         {
 
             Particle p;
+            double average_p = gRandom->Exp(1.);
             double phi = gRandom->Uniform(0, 2 * M_PI);
             double theta = gRandom->Uniform(0, M_PI);
             double impulse = gRandom->Exp(1.);
@@ -67,7 +54,7 @@ void macro2()
             double pz = impulse * cos(theta);
             double x = gRandom->Rndm();
             double y = gRandom->Rndm();
-
+//try with setIndex
             if (x < 0.4)
             {
                 p = Particle{"pi+", px, py, pz};
@@ -113,7 +100,7 @@ void macro2()
                                         p.Decay2body(d1, d2);
                                         ResonanceEvents.push_back(d1);
                                         ResonanceEvents.push_back(d2);
-                                        invmass5->Fill(d1.InvMass(d2));
+                                        hinvmass_decay->Fill(d1.InvMass(d2));
                                     }
                                     else
                                     {
@@ -122,7 +109,7 @@ void macro2()
                                         p.Decay2body(d1, d2);
                                         ResonanceEvents.push_back(d1);
                                         ResonanceEvents.push_back(d2);
-                                        invmass5->Fill(d1.InvMass(d2));
+                                        hinvmass_decay->Fill(d1.InvMass(d2));
                                     }
                                 }
                             }
@@ -131,22 +118,78 @@ void macro2()
                 }
             }
             EventParticles.push_back(p);
-            types->Fill(p.GetIndex()); // tipi di particelle generate
-            angles->Fill(phi, theta);
-            pav->Fill(average_p);
-            Impulse->Fill(sqrt(pow(px, 2) + pow(py, 2)));
-            energy->Fill(p.GetEnergy());
+            htypes->Fill(p.GetIndex());
+            hangles->Fill(phi, theta);
+            hpav->Fill(average_p);
+            hImpulse->Fill(sqrt(pow(px, 2) + pow(py, 2)));
+            henergy->Fill(p.GetEnergy());
         }
 
-        for (int m = 0; m < ResonanceEvents.size(); ++m)
+        int res_size = ResonanceEvents.size();
+
+        for (int m = 0; m < res_size; ++m)
         {
             EventParticles.push_back(ResonanceEvents[m]);
         }
-        for (int i = 0; i < EventParticles.size() - 1; i++)
+        int size = EventParticles.size();
+
+        for (int j = 0; j < size - 1; j++)
         {
+            /*
+            if (EventParticles[j].GetIndex() == 6)
+            {
+                continue;
+            }*/
+            for (int k = j + 1; k < size; k++)
+            {
+                /*
+                if (EventParticles[k].GetIndex() == 6 || j == k)
+                {
+                    continue;
+                }
+                */
+                double mass = EventParticles[j].InvMass(EventParticles[k]);
+                hinvmass0->Fill(mass);
+                if (EventParticles[j].GetCharge() * EventParticles[k].GetCharge() ==
+                    -1)
+                {
+                    hinvmass1->Fill(mass);
+                }
+                if (EventParticles[j].GetCharge() * EventParticles[k].GetCharge() ==
+                    1)
+                {
+                    hinvmass2->Fill(mass);
+                }
+                if ((EventParticles[j].GetIndex() == 0 &&
+                     EventParticles[k].GetIndex() == 3) ||
+                    (EventParticles[j].GetIndex() == 1 &&
+                     EventParticles[k].GetIndex() == 2))
+                {
+                    hinvmass3->Fill(mass);
+                }
+                if ((EventParticles[j].GetIndex() == 0 &&
+                     EventParticles[k].GetIndex() == 2) ||
+                    (EventParticles[j].GetIndex() == 1 &&
+                     EventParticles[k].GetIndex() == 3))
+                {
+                    hinvmass4->Fill(mass);
+                }
+            }
         }
+        EventParticles.clear(); // clear vectors
+        ResonanceEvents.clear();
     }
-    invmass5->Draw();
-    Impulse->Draw();
-    types->Draw();
+    //hinvmass_decay->Fit("gaus");
+    TFile *file = new TFile("particles.root", "RECREATE");
+    hpav->Write();
+    hangles->Write();
+    henergy->Write();
+    hImpulse->Write();
+    hinvmass0->Write();
+    hinvmass1->Write();
+    hinvmass2->Write();
+    hinvmass3->Write();
+    hinvmass4->Write();
+    hinvmass_decay->Write();
+    file->Close();
 }
